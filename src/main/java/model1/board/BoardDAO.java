71ce8ed49f5392887b6c8b17713e8d5a5b29ffa0
SuyeonJeong-github <success9413@naver.com>
2023-05-08 08:50:11 +0900

    08. 모델1 게시판 - 글쓰기

--- src/main/java/model1/board/BoardDAO.java
+++ src/main/java/model1/board/BoardDAO.java
@@ -77,4 +77,30 @@ public class BoardDAO extends JDBConnect{
 		
 		return bbs;
 	}
+	
+	// 게시글 데이터를 받아 DB에 추가합니다.
+	public int insertWrite(BoardDTO dto) {
+		int result = 0;
+		
+		try {
+			// insert 쿼리문 작성
+			String query = "insert into board ( "
+						+ " num, title, content, id, visitcount) "
+						+ " values ( "
+						+ " seq_board_num.nextval, ?, ?, ?, 0)";
+			
+			psmt = con.prepareStatement(query); // 동적 쿼리
+			psmt.setString(1, dto.getTitle());
+			psmt.setString(2, dto.getContent());
+			psmt.setString(3, dto.getId());
+			
+			result = psmt.executeUpdate();
+		}
+		catch(Exception e) {
+			System.out.println("게시물 입력 중 예외 발생");
+			e.printStackTrace();
+		}
+		
+		return result; // 추가한 행의 개수 반환
+	}
 }
